d7b59278ecc9676b1b2374351ffac6b226e4520b
CoiLLearningCenter <coil@coil-learning.net>
2026-02-12 17:06:00 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', padding: 24 }}>
+      <h1>openbadgecrafter</h1>
+      <p>Vite + React starter is running. We’ll connect Supabase next.</p>
+    </div>
+  )
+}
+``
