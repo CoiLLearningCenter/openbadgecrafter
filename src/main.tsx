f284e3df71e6d213ab354deb694ea746ae21b1cb
CoiLLearningCenter <coil@coil-learning.net>
2026-02-16 16:23:47 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'   // <- this must point to App.tsx
+import App from './App' // <- renders App.tsx
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
